418d49ca6790cab8c870800d981c41844eb38dde
borysjelenski <borys.jelenski@gmail.com>
2019-07-08 02:13:20 +0200

    Written unit tests for checking correctness of switching states in response to events

--- test/src/transition_test.cpp
+++ test/src/transition_test.cpp
@@ -47,3 +47,136 @@ struct Player : uhsm::State_machine<Player> {
   
   State_data_def<Transitions> state_data;
 };
+
+TEST_GROUP(Transition_TestGroup)
+{
+};
+
+TEST(Transition_TestGroup, Start_SimpleHSM_InExpectedState)
+{
+  Player sm;
+  sm.start();
+
+  CHECK(std::holds_alternative<Player::Powered_off>(sm.state_data));
+}
+
+TEST(Transition_TestGroup, React_CascadeInTr0_InExpectedState)
+{
+  Player sm;
+  sm.start();   // initialized with 'Off'
+
+  sm.react(Event::Pwr_switch_flip{});   // enters 'Powered_on::Stopped'
+
+  const bool in_state = uhsm::helpers::is_in_state<
+  Player,
+  Player::Powered_on,
+  Player::Powered_on::Stopped>(sm);
+  CHECK(in_state);
+}
+
+TEST(Transition_TestGroup, React_CascadeInTr1_InExpectedState)
+{
+  Player sm;
+  sm.start();   // initialized with 'Off'
+
+  sm.react(Event::Pwr_switch_flip{});   // enters 'Powered_on::Stopped'
+  sm.react(Event::Play_pause_btn{});    // enters 'Powered_on::Active::Playing'
+
+  const bool in_state = uhsm::helpers::is_in_state<
+  Player,
+  Player::Powered_on,
+  Player::Powered_on::Active,
+  Player::Powered_on::Active::Playing>(sm);
+  CHECK(in_state);
+}
+
+TEST(Transition_TestGroup, React_SimpleTr0_InExpectedState)
+{
+  Player sm;
+  sm.start();     // initialized with 'Off'
+
+  sm.react(Event::Pwr_switch_flip{});   // enters 'Powered_on::Stopped'
+  sm.react(Event::Play_pause_btn{});    // enters 'Powered_on::Active::Playing'
+  sm.react(Event::Play_pause_btn{});    // enters 'Powered_on::Active::Paused'
+
+  const bool in_state = uhsm::helpers::is_in_state<
+  Player,
+  Player::Powered_on,
+  Player::Powered_on::Active,
+  Player::Powered_on::Active::Paused>(sm);
+  CHECK(in_state);
+}
+
+TEST(Transition_TestGroup, React_CascadeOutTr0_InExpectedState)
+{
+  Player sm;
+  sm.start();   // initialized with 'Off'
+
+  sm.react(Event::Pwr_switch_flip{});   // enters 'Powered_on::Stopped'
+  sm.react(Event::Play_pause_btn{});    // enters 'Powered_on::Active::Playing'
+  sm.react(Event::Stop_btn{});          // leaves 'Powered_on::Active' substate, enters 'Powered_on::Stopped'
+
+  const bool in_state = uhsm::helpers::is_in_state<
+  Player,
+  Player::Powered_on,
+  Player::Powered_on::Stopped>(sm);
+  CHECK(in_state);
+}
+
+TEST(Transition_TestGroup, React_CascadeOutTr1_InExpectedState)
+{
+  Player sm;
+  sm.start();   // initialized with 'Off'
+
+  sm.react(Event::Pwr_switch_flip{});   // enters 'Powered_on::Stopped'
+  sm.react(Event::Play_pause_btn{});    // enters 'Powered_on::Active::Playing'
+  sm.react(Event::Pwr_switch_flip{});   // leaves 'Powered_on' substate, enters 'Powered_off'
+
+  const bool in_state = uhsm::helpers::is_in_state<
+  Player,
+  Player::Powered_off>(sm);
+  CHECK(in_state);
+}
+
+TEST(Transition_TestGroup, React_InternalCascadeOutTr0_InExpectedState)
+{
+  Player sm;
+  sm.start();   // initialized with 'Off'
+
+  sm.react(Event::Pwr_switch_flip{});                     // enters 'Powered_on::Stopped'
+  sm.react(Event::Play_pause_btn{});                      // enters 'Powered_on::Active::Playing'
+  const bool handled = sm.react(Event::Backward_btn{});   // defers processing to 'Powered_on::Active' substate,
+                                                          // remains in 'Powered_on::Active::Playing'
+
+  // make sure last event was actually handled and not ignored
+  CHECK(handled);
+
+  const bool in_state = uhsm::helpers::is_in_state<
+  Player,
+  Player::Powered_on,
+  Player::Powered_on::Active,
+  Player::Powered_on::Active::Playing>(sm);
+  CHECK(in_state);
+}
+
+TEST(Transition_TestGroup, React_InternalCascadeOutTr1_InExpectedState)
+{
+  Player sm;
+  sm.start();   // initialized with 'Off'
+
+  sm.react(Event::Pwr_switch_flip{});                     // enters 'Powered_on::Stopped'
+  sm.react(Event::Play_pause_btn{});                      // enters 'Powered_on::Active::Playing'
+  sm.react(Event::Play_pause_btn{});                      // enters 'Powered_on::Active::Paused'
+  const bool handled = sm.react(Event::Forward_btn{});    // defers processing to 'Powered_on::Active' substate,
+                                                          // remains in 'Powered_on::Active::Paused'
+
+  // make sure last event was actually handled and not ignored
+  CHECK(handled);
+
+  const bool in_state = uhsm::helpers::is_in_state<
+  Player,
+  Player::Powered_on,
+  Player::Powered_on::Active,
+  Player::Powered_on::Active::Paused>(sm);
+  CHECK(in_state);
+}
