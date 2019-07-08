e4d1bbf729a677b9c56a50bd72d2f43aa5721f3c
borysjelenski <borys.jelenski@gmail.com>
2019-07-08 04:46:53 +0200

    Added new transition action tests

--- test/src/action_test.cpp
+++ test/src/action_test.cpp
@@ -15,7 +15,10 @@ namespace Event
   struct Pwr_switch_flip {};
   struct Play_pause_btn {};
   struct Stop_btn {};
-  struct Forward_btn {};
+  struct Forward_btn {
+    Forward_btn(int amount_sec) : amount_sec{amount_sec} {}
+    int amount_sec;
+  };
   struct Backward_btn {};
 }
 
@@ -38,10 +41,19 @@ namespace Action
   };
 
   struct Playback_pos_forward {
-  template<typename SrcStateT, typename EventT>
+    template<typename SrcStateT, typename EventT>
     void operator()(const SrcStateT&, EventT&& evt)
     {
       mock().actualCall("playback_pos_forward");
+    }
+
+    static constexpr auto expected_sec_amount = 37;
+
+    template<typename SrcStateT>
+    void operator()(const SrcStateT&, Event::Forward_btn&& evt)
+    {
+      mock().actualCall("playback_pos_forward_w_data");
+      LONGS_EQUAL(expected_sec_amount, evt.amount_sec);
     }
   };
 
@@ -91,14 +103,70 @@ struct Player_w_actions : uhsm::State_machine<Player_w_actions> {
   State_data_def<Transitions> state_data;
 };
 
-TEST(Action_TestGroup, React_SimpleTr0_InvokeActionHandler)
+TEST(Action_TestGroup, React_CascadeTrs0_InvokeActionHandlers)
 {
   Player_w_actions sm;
-  sm.start();
+  sm.start();   // initialized with 'Powered_off'
 
+  mock().strictOrder();
   mock().expectOneCall("turn_led_on");
+  mock().expectOneCall("turn_led_off");
 
-  sm.react(Event::Pwr_switch_flip{});
+  sm.react(Event::Pwr_switch_flip{});   // enters 'Powered_on::Stopped', invokes 'turn_led_on' action
+  sm.react(Event::Pwr_switch_flip{});   // leaves 'Powered_on' substate, enters 'Powered_off',
+                                        // invokes 'turn_led_off()' action
+
+  mock().checkExpectations();
+}
+
+TEST(Action_TestGroup, React_CascadeTrs1_InvokeActionHandlers)
+{
+  Player_w_actions sm;
+  sm.start();   // initialized with 'Powered_off'
+  
+  mock().strictOrder();
+  mock().expectOneCall("turn_led_on");
+  mock().expectOneCall("turn_led_off");
+  
+  sm.react(Event::Pwr_switch_flip{});   // enters 'Powered_on::Stopped', invokes 'turn_led_on' action
+  sm.react(Event::Play_pause_btn{});    // enters 'Powered_on::Active::Playing'
+  sm.react(Event::Pwr_switch_flip{});   // leaves 'Powered_on' substate, enters 'Powered_off',
+                                        // invokes 'turn_led_off()' action
+
+  mock().checkExpectations();
+}
+
+TEST(Action_TestGroup, React_InternalTr0_InvokeActionHandlers)
+{
+  Player_w_actions sm;
+  sm.start();   // initialized with 'Powered_off'
+  
+  mock().strictOrder();
+  mock().expectOneCall("turn_led_on");
+  mock().expectOneCall("playback_pos_backward");
+
+  sm.react(Event::Pwr_switch_flip{}); // enters 'Powered_on::Stopped', invokes 'turn_led_on' action
+  sm.react(Event::Play_pause_btn{});  // enters 'Powered_on::Active::Playing'
+  sm.react(Event::Backward_btn{});    // defers processing to 'Powered_on::Active' substate, remains in 'Playing' state,
+                                      // invokes 'playback_pos_backward()' action
+
+  mock().checkExpectations();
+}
+
+TEST(Action_TestGroup, React_InternalTrWithEventData0_InvokeActionHandlersPassData)
+{
+  Player_w_actions sm;
+  sm.start();   // initialized with 'Powered_off'
+  
+  mock().strictOrder();
+  mock().expectOneCall("turn_led_on");
+  mock().expectOneCall("playback_pos_forward_w_data");
+  
+  sm.react(Event::Pwr_switch_flip{});       // enters 'Powered_on::Stopped', invokes 'turn_led_on' action
+  sm.react(Event::Play_pause_btn{});        // enters 'Powered_on::Active::Playing'
+  constexpr auto evt_data = Action::Playback_pos_forward::expected_sec_amount;
+  sm.react(Event::Forward_btn{evt_data});   // defers processing to 'Powered_on::Active' substate, remains in 'Playing' state,
+                                            // invokes 'playback_pos_forward()' action
 
   mock().checkExpectations();
 }
